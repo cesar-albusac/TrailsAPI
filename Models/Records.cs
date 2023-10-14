5d4c0f48cb2ecd04eaf7216cd1e02b84f23bf510
Cesar <Cesar@DESKTOP-E17AGI0>
2023-10-14 11:08:09 +0200

    Adding waypoint

--- Models/Records.cs
+++ Models/Records.cs
@@ -13,7 +13,7 @@
         public string Name { get; set; }
         public double DistanceInKilometers { get; set; }
         public TimeSpan EstimatedDuration { get; set; }
-        public List<string> Waypoints { get; set; }
+        public List<Waypoint> Waypoints { get; set; }
         public DifficultyLevel Difficulty { get; set; }
         public string StartingPoint { get; set; }
         public string EndingPoint { get; set; }
@@ -22,7 +22,11 @@
         public string GpxFileUrl { get; set;}
         public string[] Tags { get; set;}
 
-
+        public class Waypoint
+        {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+        }
 
         // Enum for Difficulty Level
         public enum DifficultyLevel
@@ -45,13 +49,13 @@
         }
 
         // Method to add a waypoint
-        public void AddWaypoint(string waypoint)
+        public void AddWaypoint(Waypoint waypoint)
         {
             Waypoints.Add(waypoint);
         }
 
         // Method to remove a waypoint
-        public void RemoveWaypoint(string waypoint)
+        public void RemoveWaypoint(Waypoint waypoint)
         {
             Waypoints.Remove(waypoint);
         }
